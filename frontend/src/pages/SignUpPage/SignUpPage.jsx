abf21cfc473b0c31c060f2364bf1a4b8764f48b4
wil-l-l <wsaintlouiswil@gmail.com>
2026-03-17 15:57:20 -0400

    Add name input elements and form submission button to SignUpPage

--- frontend/src/pages/SignUpPage/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage/SignUpPage.jsx
@@ -1,5 +1,20 @@
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+      action=""
+    >
+      <input type="text" placeholder="First Name" />
+      <br />
+      <input type="text" placeholder="Last Name" />
+      <br />
+      <input type="text" placeholder="Middle Name" />
+      <br />
+      <button>Submit</button>
+    </form>
+  );
 };
 
 export default SignUpPage;

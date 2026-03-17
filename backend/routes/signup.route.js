7b949698ae5194d4d23b0bbe1c9ddad21bb2f481
wil-l-l <wsaintlouiswil@gmail.com>
2026-03-16 22:16:02 -0400

    Add school code to the username of new users

--- backend/routes/signup.route.js
+++ backend/routes/signup.route.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user.model");
+const School = require("../models/school.model");
 
 router.post("/", async (req, res) => {
-  const { firstName, lastName, middleName } = req.body;
+  const { firstName, lastName, middleName, school } = req.body;
   let newStudent = new User({
     firstName,
     lastName,
-    userName: (lastName + firstName[0]).toLowerCase(),
   });
   if (middleName) newStudent.middleName = middleName;
 
+  const schoolFound = await School.find().or([
+    { name: school },
+    { code: school },
+  ]); // the value of 'school' passed by the client could be the school name or school code
+
+  newStudent.userName =
+    (lastName + firstName[0]).toLowerCase() + schoolFound[0].code;
+
   newStudent = await newStudent.save();
 
-  res.status(201).send({ success: true, message: "Created a new user" });
+  res
+    .status(201)
+    .send({ success: true, message: "Created a new user", data: newStudent });
 });
 
 module.exports = router;

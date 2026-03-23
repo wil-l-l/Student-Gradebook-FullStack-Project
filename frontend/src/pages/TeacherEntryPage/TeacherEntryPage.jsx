e9d223477dc0efe52eeb70c814a8a2112df6e626
wil-l-l <wsaintlouiswil@gmail.com>
2026-03-23 01:13:52 -0400

    Create 'ManageCoursePage' component and use within 'TeacherEntryPage'

--- frontend/src/pages/TeacherEntryPage/TeacherEntryPage.jsx
+++ frontend/src/pages/TeacherEntryPage/TeacherEntryPage.jsx
@@ -2,41 +2,22 @@ import { useContext } from "react";
 import "./TeacherEntryPage.css";
 import { UserContext } from "../../contexts/UserContext";
 import { useState } from "react";
+import ManageCoursePage from "../../components/ManageCoursePage/ManageCoursePage";
 
 const TeacherEntryPage = () => {
   const teacher = useContext(UserContext);
-  const [selectedClass, setSelectedClass] = useState(null);
+  const [selectedCourse, setSelectedCourse] = useState(null);
 
   return (
     <>
       <main>
-        {selectedClass ? (
-          <>
-            <h2 className="teacher-entry-page__heading">
-              Class Opened: {selectedClass}
-            </h2>
-            <div
-              onClick={(e) => {
-                console.log("assignments");
-              }}
-              className="teacher-entry-page__prompt-block"
-            >
-              Assignments
-            </div>
-            <div
-              onClick={(e) => {
-                console.log("students");
-              }}
-              className="teacher-entry-page__prompt-block"
-            >
-              Students
-            </div>
-          </>
+        {selectedCourse ? (
+          <ManageCoursePage selectedCourse={selectedCourse} />
         ) : (
           <>
             <h2 className="teacher-entry-page__heading">Classes</h2>
             <ul className="teacher-entry-page__classes-list">
-              {teacher.courses.map(({ name, period, students }) => (
+              {teacher.courses.map(({ name, period, students }, index) => (
                 <li
                   key={period}
                   className={`teacher-entry-page__classes-list__item${
@@ -44,9 +25,9 @@ const TeacherEntryPage = () => {
                       ? " teacher-entry-page__classes-list__item--active"
                       : " teacher-entry-page__classes-list__item--inactive"
                   }`}
-                  onClick={(e) => {
+                  onClick={() => {
                     if (!(students.length > 0)) return;
-                    setSelectedClass(name);
+                    setSelectedCourse(teacher.courses[index]);
                   }}
                 >
                   {name} {period}

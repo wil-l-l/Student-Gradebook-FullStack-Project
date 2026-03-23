6cb8e3a1ae6ce20f4367d756f29467b8f1b88993
wil-l-l <wsaintlouiswil@gmail.com>
2026-03-23 00:26:39 -0400

    Add 'selectedClass' state to 'TeacherEntryPage' different markup returns based on whether or not a class item has been selected

--- frontend/src/pages/TeacherEntryPage/TeacherEntryPage.jsx
+++ frontend/src/pages/TeacherEntryPage/TeacherEntryPage.jsx
@@ -1,32 +1,60 @@
 import { useContext } from "react";
 import "./TeacherEntryPage.css";
 import { UserContext } from "../../contexts/UserContext";
+import { useState } from "react";
 
 const TeacherEntryPage = () => {
   const teacher = useContext(UserContext);
+  const [selectedClass, setSelectedClass] = useState(null);
 
   return (
     <>
       <main>
-        <h2 className="teacher-entry-page__heading">Classes</h2>
-        <ul className="teacher-entry-page__classes-list">
-          {teacher.courses.map(({ name, period, students }) => (
-            <li
-              key={period}
-              className={`teacher-entry-page__classes-list__item${
-                students.length > 0
-                  ? " teacher-entry-page__classes-list__item--active"
-                  : " teacher-entry-page__classes-list__item--inactive"
-              }`}
+        {selectedClass ? (
+          <>
+            <h2 className="teacher-entry-page__heading">
+              Class Opened: {selectedClass}
+            </h2>
+            <div
               onClick={(e) => {
-                if (!(students.length > 0)) return;
-                console.log(`Clicked on ${name} class block`);
+                console.log("assignments");
               }}
+              className="teacher-entry-page__prompt-block"
             >
-              {name} {period}
-            </li>
-          ))}
-        </ul>
+              Assignments
+            </div>
+            <div
+              onClick={(e) => {
+                console.log("students");
+              }}
+              className="teacher-entry-page__prompt-block"
+            >
+              Students
+            </div>
+          </>
+        ) : (
+          <>
+            <h2 className="teacher-entry-page__heading">Classes</h2>
+            <ul className="teacher-entry-page__classes-list">
+              {teacher.courses.map(({ name, period, students }) => (
+                <li
+                  key={period}
+                  className={`teacher-entry-page__classes-list__item${
+                    students.length > 0
+                      ? " teacher-entry-page__classes-list__item--active"
+                      : " teacher-entry-page__classes-list__item--inactive"
+                  }`}
+                  onClick={(e) => {
+                    if (!(students.length > 0)) return;
+                    setSelectedClass(name);
+                  }}
+                >
+                  {name} {period}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </main>
     </>
   );

971afcd05aeb1aded723a8fb9b7515bce7298f09
wil-l-l <wsaintlouiswil@gmail.com>
2026-03-22 23:46:41 -0400

    Render teacher's classes on 'TeacherEntryPage' and add styling to the page

--- frontend/src/pages/TeacherEntryPage/TeacherEntryPage.jsx
+++ frontend/src/pages/TeacherEntryPage/TeacherEntryPage.jsx
@@ -1,7 +1,24 @@
+import { useContext } from "react";
 import "./TeacherEntryPage.css";
+import { UserContext } from "../../contexts/UserContext";
 
 const TeacherEntryPage = () => {
-  return <div>TeacherEntryPage</div>;
+  const teacher = useContext(UserContext);
+
+  return (
+    <>
+      <main>
+        <h2 className="teacher-entry-page__heading">Classes</h2>
+        <ul className="teacher-entry-page__classes-list">
+          {teacher.courses.map(({ name, period }) => (
+            <li key={period} className="teacher-entry-page__classes-list__item">
+              {name} {period}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
 };
 
 export default TeacherEntryPage;

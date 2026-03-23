39b5eebd8f9fe3927f8c2cf262b898f6d1659833
wil-l-l <wsaintlouiswil@gmail.com>
2026-03-23 00:07:21 -0400

    Add onClick event handler to each class list item in 'TeacherEntryPage' and check if a class list item has students or not

--- frontend/src/pages/TeacherEntryPage/TeacherEntryPage.jsx
+++ frontend/src/pages/TeacherEntryPage/TeacherEntryPage.jsx
@@ -10,8 +10,19 @@ const TeacherEntryPage = () => {
       <main>
         <h2 className="teacher-entry-page__heading">Classes</h2>
         <ul className="teacher-entry-page__classes-list">
-          {teacher.courses.map(({ name, period }) => (
-            <li key={period} className="teacher-entry-page__classes-list__item">
+          {teacher.courses.map(({ name, period, students }) => (
+            <li
+              key={period}
+              className={`teacher-entry-page__classes-list__item${
+                students.length > 0
+                  ? " teacher-entry-page__classes-list__item--active"
+                  : " teacher-entry-page__classes-list__item--inactive"
+              }`}
+              onClick={(e) => {
+                if (!(students.length > 0)) return;
+                console.log(`Clicked on ${name} class block`);
+              }}
+            >
               {name} {period}
             </li>
           ))}

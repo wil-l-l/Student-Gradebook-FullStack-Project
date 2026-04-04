191f2184c7d97795737a18ab3b8569eee98e65fe
wil-l-l <wsaintlouiswil@gmail.com>
2026-04-04 15:25:01 -0400

    fix: Fix 'getGradePercentage' rounding error

--- frontend/src/utils/getGradePercentage.js
+++ frontend/src/utils/getGradePercentage.js
@@ -1,6 +1,6 @@
 import getRoundedGrade from "./getRoundedGrade";
 
 const getGradePercentage = (grade) =>
-  isNaN(grade) ? `0%` : `${getRoundedGrade(grade) * 100}%`;
+  isNaN(grade) ? `0%` : `${(getRoundedGrade(grade) * 100).toFixed()}%`;
 
 export default getGradePercentage;

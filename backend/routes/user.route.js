4f8dd8770745361090e193511d0627f808126798
wil-l-l <wsaintlouiswil@gmail.com>
2026-04-17 21:02:33 -0400

    feat: Add id validation to user route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const { User } = require("../models/user.model");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
+  if (!id)
+    return res
+      .status(400)
+      .send({ success: false, message: "No id in request" });
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res
+      .status(404)
+      .send({ success: false, message: "Invalid id passed" });
 
   const user = await User.findById(id);
+  if (!user)
+    return res
+      .status(404)
+      .send({ success: false, message: "Could not get user" });
 
   res.status(200).send({ success: true, data: user });
 });

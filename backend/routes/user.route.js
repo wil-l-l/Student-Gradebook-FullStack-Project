48375be7bd667f0fae8ad4228b637845ee2835da
wil-l-l <wsaintlouiswil@gmail.com>
2026-04-17 20:54:12 -0400

    Change router.use to router.get in user route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { User } = require("../models/user.model");
 const router = express.Router();
 
-router.use("/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
 
   const user = await User.findById(id);

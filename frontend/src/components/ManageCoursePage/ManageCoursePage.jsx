2b551499a7eb0709fa00057a7261b09a4543d26a
wil-l-l <wsaintlouiswil@gmail.com>
2026-04-20 22:58:15 -0400

    Change look of 'ManageCoursePage'

--- frontend/src/components/ManageCoursePage/ManageCoursePage.jsx
+++ frontend/src/components/ManageCoursePage/ManageCoursePage.jsx
@@ -3,6 +3,7 @@ import getCourseFromPeriod from "../../utils/getCourseFromPeriod";
 import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import "./ManageCoursePage.css";
+import PageCard from "../PageCard/PageCard";
 
 const ManageCoursePage = () => {
   const { user } = useContext(UserContext);
@@ -11,23 +12,29 @@ const ManageCoursePage = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
+  const getNavigateHandler = (endpoint) => {
+    navigate(`/teacher/course/${period}/${endpoint}`);
+  };
+
   return (
     <>
       {pathname === `${pathname}` && (
         <>
-          <h2>Class Opened: {course.name}</h2>
-          <div
-            onClick={() => navigate(`/teacher/course/${period}/assignments`)}
-            className="manage_course_page__prompt-block"
-          >
-            Assignments
-          </div>
-          <div
-            onClick={() => navigate(`/teacher/course/${period}/students`)}
-            className="manage_course_page__prompt-block"
-          >
-            Students
-          </div>
+          <PageCard
+            headingBoxChildren={<h2>Viewing: {course.name}</h2>}
+            list={[
+              {
+                key: "assignments",
+                children: `Assignments`,
+                onClickHandler: () => getNavigateHandler("assignments"),
+              },
+              {
+                key: "students",
+                children: `Students`,
+                onClickHandler: () => getNavigateHandler("students"),
+              },
+            ]}
+          />
         </>
       )}
     </>

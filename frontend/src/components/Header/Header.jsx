5d955609e839fa77dc1c76a51911bfa74072112d
wil-l-l <wsaintlouiswil@gmail.com>
2026-04-23 00:43:24 -0400

    Add profile icon to student entry page

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import "./Header.css";
 import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import LogoutBtn from "../LogoutBtn/LogoutBtn";
+import ProfileIcon from "../../assets/icons/profile-icon.png";
 
 const Header = () => {
   const { user } = useContext(UserContext);
@@ -10,7 +11,9 @@ const Header = () => {
   return (
     <header className="header-box">
       <div className="profile-box">
-        <img src="" alt={fullName} className="profile-img" />
+        <div className="profile-img-box">
+          <img src={ProfileIcon} alt={fullName} className="profile-img" />
+        </div>
         <p className="profile-name">{fullName}</p>
       </div>
       <LogoutBtn />

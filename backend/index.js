ea5d1bbcf66ba45389f3d6071646d3087de94b86
wil-l-l <wsaintlouiswil@gmail.com>
2026-05-05 17:00:52 -0400

    Install and implement production modules and code in the backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const config = require("config");
+const cors = require("cors");
+
+process.on("uncaughtException", (err) => {
+  console.error("Caught an uncaught exception: ", ex);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("Caught an unhandled rejection: ", reason);
+  process.exit(1);
+});
+
+if (!config.get("db")) {
+  console.error("Could not start app: db is not defined");
+  process.exit(1);
+}
 
 mongoose
   .connect("mongodb://localhost/gradebook")
   .then(() => console.log(`Connected to mongodb`))
   .catch((err) => console.error(`Could not connect to mongodb:`, err.message));
 
+app.use(cors());
+require("./startup/production")(app);
 require("./startup/routes")(app);
 
 const PORT = process.env.PORT || 3000;
